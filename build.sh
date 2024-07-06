9abd9e819b5545e6c8a5fb2b309d924fcb4b4b60
Ben Riegel <ben.r@outlook.de>
2024-07-06 21:34:21 +0800

    fix typo

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd simple-client || exit
 docker buildx build --platform linux/amd64 -t mrsarius/simple-client:latest --push .
 cd ..
-cd simple-client || exit
+cd simple-server || exit
 docker buildx build --platform linux/amd64 -t mrsarius/simple-server:latest --push .
