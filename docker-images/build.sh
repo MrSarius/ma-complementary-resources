1955709c9c7fd306afc122287a54cb8d1be5a09a
Ben Riegel <ben.riegel@tum.de>
2024-07-16 17:03:21 +0000

    add debug docker container

--- docker-images/build.sh
+++ docker-images/build.sh
@@ -1,3 +1,6 @@
+cd debug || exit
+docker buildx build --platform linux/amd64 -t mrsarius/debug:latest --push .
+cd ..
 cd simple-client || exit
 docker buildx build --platform linux/amd64 -t mrsarius/simple-client:latest --push .
 cd ..
