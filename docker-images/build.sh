ccb9c1f46f44d90ac038c690c3772324d57a0263
Ben Riegel <ben.riegel@tum.de>
2024-07-17 12:00:13 +0000

    make simple server and client TCP instead of UDP

--- docker-images/build.sh
+++ docker-images/build.sh
@@ -6,3 +6,7 @@ docker buildx build --platform linux/amd64 -t mrsarius/simple-client:latest --pu
 cd ..
 cd simple-server || exit
 docker buildx build --platform linux/amd64 -t mrsarius/simple-server:latest --push .
+
+sudo ctr -n oakestra images pull docker.io/mrsarius/debug:latest
+sudo ctr -n oakestra images pull docker.io/mrsarius/simple-server:latest
+sudo ctr -n oakestra images pull docker.io/mrsarius/simple-client:latest
